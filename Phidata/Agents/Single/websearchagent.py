cd819a581c234cc3b4fe38d8c0177b78435e8305
Prathamesh Laxman kashid <74651487+Prathamesh148@users.noreply.github.com>
2025-03-21 15:30:31 +0530

    Add files via upload

---
+++ Phidata/Agents/Single/websearchagent.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 21 15:25:17 2025
+
+@author: Prathamesh Kashid
+"""
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.tools.duckduckgo import DuckDuckGo
+
+web_agent = Agent(
+    name="Web Agent",
+    model=OpenAIChat(id="gpt-4o"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+)
+web_agent.print_response("Tell me about OpenAI Sora?", stream=True)
